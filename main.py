977bca5b8791da5a2b85541d04033d3bf9e248a8
L-ingqin12 <2806428415@qq.com>
2023-04-25 23:48:18 +0800

    main

---
+++ main.py
@@ -0,0 +1,29 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# 知乎user信息
+# https://www.zhihu.com/api/v4/members/chen-xin-36-32-62?include=follower_count,voteup_count,favorited_count,thanked_count%27
+
+# https://zhuanlan.zhihu.com/p/87029765
+
+# https://pyqt-fluent-widgets.readthedocs.io/zh_CN/latest/quick-start.html
+
+# https://www.zhihu.com/api/v3/explore/guest/feeds?limit=100
+
+# https://api.zhihu.com/people/eed5d99dfd8d987f2346852460ba8071
+
+# https://www.zhihu.com/billboard
